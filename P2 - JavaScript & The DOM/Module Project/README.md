fc036799ae8956963db6c1746ae0e94396bfe462
RanchesterTalon <ranchestertalon@gmail.com>
2021-03-16 12:27:10 +0300

    feat: Add interactivity to static site

--- P2 - JavaScript & The DOM/Module Project/README.md
+++ P2 - JavaScript & The DOM/Module Project/README.md
@@ -1,13 +1,5 @@
 # Landing Page Project
 
-## Table of Contents
+An interactive website that dynamically builds a navbar for each section in the HTML file. 
 
-* [Instructions](#instructions)
-
-## Instructions
-
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+The navbar lets the user jump through the sections using a smooth scrolling motion.
--- P2 - JavaScript & The DOM/Module Project/js/app.js
+++ P2 - JavaScript & The DOM/Module Project/js/app.js
@@ -17,14 +17,23 @@
  * Define Global Variables
  * 
 */
-
+let sectionList = document.getElementsByTagName("section"),
+navBar = document.getElementById("navbar__list");
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-
+// Helper function developed using code from https://www.javascripttutorial.net/dom/css/check-if-an-element-is-visible-in-the-viewport/
+function inViewport(element) {
+    const rectObject = element.getBoundingClientRect();
+    return(
+        (rectObject.top <= 0) &&
+        rectObject.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rectObject.bottom > 0
+    );
+}
 
 
 /**
@@ -34,13 +43,34 @@
 */
 
 // build the nav
-
+function navBuild() {
+    for (let i = 0; i < sectionList.length; i++) {
+        navBar.innerHTML += "<li id=\"nav" + sectionList[i].dataset.nav.split(" ")[1] + "\"><a href=#" + sectionList[i].id + " class=\"menu__link\">" + sectionList[i].dataset.nav + "</a></li>";
+    }
+}
 
 // Add class 'active' to section when near top of viewport
-
+function activeClass(event) {
+    for (let i = 0; i < sectionList.length; i++) {
+        if (inViewport(sectionList[i])) {
+            sectionList[i].className = "activeSection";
+            document.getElementById(`nav${i+1}`).classList.add("activeMenu");
+        } else {
+            sectionList[i].removeAttribute("class");
+            document.getElementById(`nav${i+1}`).classList.remove("activeMenu");
+        }
+    }
+        
+}
 
 // Scroll to anchor ID using scrollTO event
-
+function anchorScroll(event) {
+    event.preventDefault();
+    const section = document.getElementById(this.getAttribute("href").slice(1)),
+    xCoord = section.offsetLeft,
+    yCoord = section.offsetTop;
+    window.scrollTo({top:yCoord, left:xCoord, behavior: "smooth"});
+}
 
 /**
  * End Main Functions
@@ -49,9 +79,12 @@
 */
 
 // Build menu 
-
+navBuild();
 // Scroll to section on link click
-
+const anchors = document.querySelectorAll("li > a");
+for (const anchor of anchors) {
+    anchor.addEventListener("click", anchorScroll);
+}
 // Set sections as active
-
+document.addEventListener("scroll", activeClass);
 
